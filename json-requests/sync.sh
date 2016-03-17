a0ee4ee5ce9120fb21cc0edde38039408b7049d4
Nikolay Volf <nikvolf@gmail.com>
2016-03-17 18:26:51 +0100

    Update sync.sh

--- json-requests/sync.sh
+++ json-requests/sync.sh
@@ -1,1 +1,8 @@
+#### sync stat
 curl -X POST --data '{"jsonrpc":"2.0","method":"eth_syncing","params":[],"id":1}' http://0.0.0.0:8545
+#### pending transactions
+curl -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockTransactionCountByNumber","params":["pending"],"id":1}' http://0.0.0.0:8545
+#### pending transactions filter
+curl -X POST --data '{"jsonrpc":"2.0","method":"eth_newPendingTransactionFilter","params":[],"id":1}' http://0.0.0.0:8545
+#### 
+curl -X POST --data '{"jsonrpc":"2.0","method":"eth_getFilterChanges","params":["0x0144444"],"id":1}' http://0.0.0.0:8545
