e13069ff1451b6ad0bd1d0fc03844bc502ab5c6c
Nikolay Volf <nikvolf@gmail.com>
2016-10-04 13:01:01 +0300

    sample

--- web3/blocks.js
+++ web3/blocks.js
@@ -4,8 +4,11 @@ var web3 = new Web3();
 var ipc_path = '/home/nikvolf/.parity/jsonrpc.ipc';
 web3.setProvider(new web3.providers.IpcProvider(ipc_path, net));
 
-for (var i = 0; i < 2000000; i++) {
-  web3.eth.getBlock(i, true, function( err, block ) {
-    console.log("block " + i + " ok");
-  });
+var next = 0;
+
+function next_block(err, block) {
+	err && console.err(err) || (block && console.log("block " + next + " ok"));
+	web3.eth.getBlock(next++, true, next_block);
 }
+
+next_block(false, false);
